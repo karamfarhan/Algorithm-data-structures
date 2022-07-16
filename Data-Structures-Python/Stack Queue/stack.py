5104baf02cd2b50c637df0402b9424e3e739560e
karamfarhan <www.karam777krm@gmail.com>
2022-07-16 20:50:44 +0300

    data structure queue

--- Data-Structures-Python/Stack Queue/stack.py
+++ Data-Structures-Python/Stack Queue/stack.py
@@ -14,3 +14,13 @@ class stack:
 		return self.cont[-1]
 	def is_empty(self):
 		return len(self.cont) == 0
+
+
+
+
+# st = stack()
+# st.push(5)
+# st.push(8)
+# st.push(3)
+# print (st.pop())
+# print (st.lass_item())
