51c0a7decda6e7132e5919b86241f641c07c8f23
karamfarhan <www.karam777krm@gmail.com>
2022-07-16 21:03:54 +0300

    linked list - add remove_at, insert_at methods

--- Data-Structures-Python/linked-list/linked-list.py
+++ Data-Structures-Python/linked-list/linked-list.py
@@ -36,7 +36,51 @@ class LinkedList:
         for i in data_list:
             self.insert_at_end(i)
 
+    # to return the lenth of the linked list
 
+    def lenth(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count += 1
+            itr = itr.next
+        return count
+
+    # to remove element at a given index
+
+    def remove_at(self, index):
+        if index < 0 or index > self.lenth():
+            raise Exception('invilid index')
+        if index == 0:
+            self.head = self.head.next
+
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count += 1
+
+    # to insert elemrent at a given index
+
+    def insert_at(self, index, data):
+        if index < 0 or index > self.lenth():
+            raise Exception("invilid index")
+
+        if index == 0:
+            self.insert_at_begin(data)
+            return
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index - 1:
+                node = Node(data, itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count += 1
 
 
 
