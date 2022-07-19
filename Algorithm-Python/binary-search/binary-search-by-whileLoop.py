a4c43ba8f82a625a18fdc501905a09cc068aa839
karamfarhan <www.karam777krm@gmail.com>
2022-07-19 19:30:31 +0300

    binary search
    - add? the function
    - add? some test code

---
+++ Algorithm-Python/binary-search/binary-search-by-whileLoop.py
@@ -0,0 +1,27 @@
+def binary(to_found, l):
+    low_left = 0
+    high_right = (len(l)-1)
+    mid_item = 0
+    nub_try = 0
+    found_if = False
+    while not found_if:        # we can use this code  if mid_item > len(l):
+        if low_left > high_right:   			# and put it after define mid_item(K)
+            print("Not Found")
+            break
+
+        mid_item = low_left+((high_right-low_left)//2)
+        # (K) here
+        nub_try += 1
+        if l[mid_item] == to_found:
+            print("Number Is Found")
+            print(f"{nub_try} Try To Find {l[mid_item]}")
+            found_if = True
+        if l[mid_item] < to_found:
+            low_left = mid_item + 1
+        if l[mid_item] > to_found:
+            high_right = mid_item - 1
+
+
+l = [i for i in range(25000000)]
+# l = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+binary(20000000, l)
